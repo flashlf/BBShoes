749cc57a69f3495c4fc35415321deba4615ce7ad
Flash Lavon <flash.lavon@hotmail.com>
2020-12-04 20:53:31 +0700

    Penambahan fitur delete pada home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../shared/Appointment';
+import { AppointmentService } from './../shared/appointment.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  Bookings = [];
 
-  constructor() {}
+  constructor( private aptService : AppointmentService ) {}
 
+  ngOnInit() {
+    this.fetchBookings();
+    let bookingRes = this.aptService.getBookingList();
+    bookingRes.snapshotChanges().subscribe(res => {
+      this.Bookings = [];
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$key'] = item.key;
+        this.Bookings.push(a as Appointment);
+      })
+    })
+  }
+
+  fetchBookings() {
+    this.aptService.getBookingList().valueChanges().subscribe(res => {
+      console.log(res)
+    })
+  }
+
+  deleteBooking(id) {
+    console.log(id)
+    if (window.confirm('Do you really want to delete?')) {
+      this.aptService.deleteBooking(id);
+    }
+  }
 }
