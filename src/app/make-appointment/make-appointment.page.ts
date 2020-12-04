5c05366165c25496d7c14d3acfb9f9e8aa164c75
Flash Lavon <flash.lavon@hotmail.com>
2020-12-04 20:43:42 +0700

    Fungsi Add Item di Firebase Database

--- src/app/make-appointment/make-appointment.page.ts
+++ src/app/make-appointment/make-appointment.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AppointmentService } from './../shared/appointment.service';
 
 @Component({
   selector: 'app-make-appointment',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./make-appointment.page.scss'],
 })
 export class MakeAppointmentPage implements OnInit {
+  bookingForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private aptService: AppointmentService,
+    private router: Router,
+    public fb: FormBuilder
+    ) {  }
 
   ngOnInit() {
+    this.bookingForm = this.fb.group({
+      name    : [''],
+      email   : [''],
+      mobile  : ['']
+    })
   }
 
+  formSubmit() {
+    if (!this.bookingForm.valid) {
+      return false;
+    } else {
+      this.aptService.createBooking(this.bookingForm.value).then(res => {
+        console.log(res)
+        this.bookingForm.reset();
+        this.router.navigate(['/home']);
+      }).catch(err => console.log(err))    
+    }
+  }
 }
