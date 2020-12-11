e193a523a1cee921625fcacc46b48e7c7c230f51
Flash Lavon <flash.lavon@hotmail.com>
2020-12-11 21:47:58 +0700

    Desain halaman Product

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.page.scss'],
 })
 export class ProductPage implements OnInit {
-
-  constructor() { }
+  hidden : boolean;
+  iconName : String;
+  constructor() { 
+    this.hidden = true;
+    this.iconName = "filter-circle-outline";
+  }
 
   ngOnInit() {
   }
 
+  public showSearch(): void{
+    console.log("Search dipencet")
+    if(this.hidden == true) {
+      this.hidden = false;
+    } else {
+      this.hidden = true;
+    }
+  }
+  public showFilter(): void {
+    
+    console.log("Filter dipencet")
+    if(this.iconName == "filter-circle-outline") {
+      this.iconName = "filter-circle-sharp";
+    } else {
+      this.iconName = "filter-circle-outline";
+    }
+  }
 }
