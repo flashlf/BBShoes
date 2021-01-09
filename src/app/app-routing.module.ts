d197c81c096cffcb0f7e6abfaa79339e6e0af584
Flash Lavon <flash.lavon@hotmail.com>
2021-01-09 14:34:29 +0700

    Penambahan halaman Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,6 +27,10 @@ const routes: Routes = [
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
 ];
 
 @NgModule({
